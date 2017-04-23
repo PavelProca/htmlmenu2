c7a2df333d48ff9d4af3da83b45ce407aa253fc6
Proca Pavel <proka.pavel@gmail.com>
2017-04-23 21:37:43 +0300

    123

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-function check(menu, label) {
-	switch(menu) {
-		case 1: 
-			scheck=false;
-			document.getElementsByTagName('h1')[0].innerHTML = label;
-			document.getElementsByClassName('submenu')[0].classList.remove("active");
-			document.getElementsByClassName('submenu')[1].classList.remove("active");
-			break;
-		case 2:
-			document.getElementsByClassName('submenu')[1].classList.remove("active");
-			document.getElementsByClassName('submenu')[0].classList.add("active");
-			document.getElementsByTagName('h1')[0].innerHTML = label;
-			break;
-		case 3:
-			document.getElementsByClassName('submenu')[0].classList.remove("active");
-			document.getElementsByClassName('submenu')[1].classList.add("active");
-			document.getElementsByTagName('h1')[0].innerHTML = label;
-			break;
-		case 2.1: case 2.2: case 2.3: 
-		case 3.1: case 3.2: case 3.3:
-			document.getElementsByTagName('h1')[0].innerHTML = label;
-			break;
-		default: return;
-	}
-}
-
 function hide(act) {
 	document.getElementsByClassName('side-menu')[0].style.transition = 'all 0.3s ease';
 	document.getElementsByClassName('main-wrapper')[0].style.transition = 'all 0.3s ease';
@@ -42,10 +16,33 @@ function hide(act) {
 	}
 }
 
+function check(menu, label) {
+	switch(menu) {
+		case 1: 
+			scheck=false;
+			document.getElementsByTagName('h1')[0].innerHTML = label;
+			document.getElementsByClassName('submenu')[0].classList.remove("active");
+			document.getElementsByClassName('submenu')[1].classList.remove("active");
+			break;
+		case 2:
+			document.getElementsByClassName('submenu')[1].classList.remove("active");
+			document.getElementsByClassName('submenu')[0].classList.add("active");
+			break;
+		case 3:
+			document.getElementsByClassName('submenu')[0].classList.remove("active");
+			document.getElementsByClassName('submenu')[1].classList.add("active");
+			break;
+		case 2.1: case 2.2: case 2.3: 
+		case 3.1: case 3.2: case 3.3:
+			document.getElementsByTagName('h1')[0].innerHTML = label;
+			break;
+		default: return;
+	}
+}
+
 function another(im) {
 	switch(im) {
 		case 2.1:
-			document.getElementById('elem2.1').checked = 'true';
 			document.getElementById('pic').src = 'images/users.jpg';
 			break;
 		case 2.2:
@@ -55,7 +52,6 @@ function another(im) {
 			document.getElementById('pic').src = 'images/roles.jpg';
 			break;
 		case 3.1:
-			document.getElementById('elem3.1').checked = 'true';
 			document.getElementById('pic').src = 'images/documents.jpg';
 			break;
 		case 3.2:
